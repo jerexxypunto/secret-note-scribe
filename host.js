7b76c7382c50beb5a16f86bce9726f87f6b47944
Jeremías San Martín <=>
2025-04-13 23:14:49 -0400

    refactor: reorganize redirection logic for improved readability

--- host.js
+++ host.js
@@ -4,10 +4,21 @@ const redirectUrl = {
 };
 
 function redriectTo(){
-    const url = new URL(location.href);
-    const pathname = url.pathname;
 
-    if (pathname === '/index.html' || pathname === '/') {
+    function path_compare( route ){
+
+        const url = new URL(location.href);
+        const pathname = url.pathname;
+
+        if (pathname === '/index.html' || pathname === '/' || pathname === route ) {
+            return true;
+        }else{
+            return false;
+        }
+
+    }
+
+    if (path_compare( '/secret-note-scribe/' )) {
         // Redirect to the app page
         location.href = redirectUrl.app;
     }
